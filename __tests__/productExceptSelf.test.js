7f7654694d4e5bf444ee5783725f3daa46e23a72
npereznyc <natalieperez84@gmail.com>
2023-06-27 12:21:51 -0400

    Added tests for productExceptSelf

---
+++ __tests__/productExceptSelf.test.js
@@ -0,0 +1,18 @@
+const { test } = require('node:test')
+const productExceptSelf = require('../productExceptSelf')
+
+test('Test with positive numbers', ()=> {
+    expect(productExceptSelf([1,2,3,4])).toEqual([24,12,8,6])
+})
+
+test('Test with negative numbers and zero', ()=>{
+    expect(productExceptSelf([-1,1,0,-3,3])).toEqual([0,0,9,0,0])
+})
+
+test('Test with one element', ()=> {
+    expect(productExceptSelf([1])).toEqual([NaN])
+})
+
+test('Test with two elements', ()=> {
+    expect(productExceptSelf([1,2])).toEqual([2,1])
+})
---
+++ productExceptSelf.js
@@ -0,0 +1,5 @@
+function productExceptSelf(){
+
+}
+
+module.exports = productExceptSelf;
