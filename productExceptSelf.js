68010996fd53176cc2d4215b217ba56209d4b62a
npereznyc <natalieperez84@gmail.com>
2023-06-27 12:57:59 -0400

    solution for productExceptSelf

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -1,5 +1,28 @@
-function productExceptSelf(){
+function productExceptSelf(nums) {
 
+    if(nums.length ===1){
+        return [NaN]
+    }
+    let answer = []
+
+    let leftProduct = 1
+    for (let i = 0; i < nums.length; i++) {
+        answer[i] = leftProduct
+        leftProduct *= nums[i]
+    }
+
+    let rightProduct = 1
+    for (let i = nums.length - 1; i >= 0; i--) {
+        answer[i] *= rightProduct
+        rightProduct *= nums[i]
+    }
+
+    for(let i=0; i<answer.length; i++){
+        if(answer[i]===-0){
+            answer[i]=0
+        }
+    }
+    return answer
 }
 
 module.exports = productExceptSelf;
