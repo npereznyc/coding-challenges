7a8ad47b0b025bea55f46b0b5a23dbc8978a824a
npereznyc <natalieperez84@gmail.com>
2023-07-27 15:29:17 -0400

    Added comments on plusOne since solution is failing some testcases

--- plusOne.js
+++ plusOne.js
@@ -7,10 +7,16 @@ function plusOne(digits) {
     //convert integer to string
     intString=int.toString()
     //convert string to array and map over that array, converting each string element to a number:
-    result=intString.split('').map((item)=>parseInt(item,10))
-    
+    result=intString.split('').map((item)=>parseInt(item))
+
     return result
 }
 plusOne([1,2,3])
 
 module.exports = plusOne;
+
+//This solution only passes 71 / 111 testcases. Does not work with much larger numbers. For example:
+//digits=[6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3] 
+//should return [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4] 
+//but is returning [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,0,0,0]
+
